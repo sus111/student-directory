81b3c9b162fdfed1527ca1fe8128c7ee7933f264
sus111 <sgraham535@gmail.com>
2016-06-20 12:29:01 +0100

    add user input to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Witch of the West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Krueger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name: "Norman Bates", cohort: :november}
-]
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice."
+
+students = []
+name = gets.chomp
+
+while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+end
+students
+end
+
 def print_header
-   puts "The students of the Villains Academy"
+   puts "The students of my cohort at Makers Academy"
    puts "-------------"
 end
 
@@ -26,6 +28,22 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students."
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+#students = [
+# {name: "Dr. Hannibal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Krueger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Joffrey Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+#]
+
